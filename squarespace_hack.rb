af26e4f800a3f22856c43a319694fb40871545f1
Stéphane Akkaoui <stephane.akkaoui@imeuble.info>
2009-06-10 10:46:47 +0200

    Add the wonderfull script that will make you an iPhone owner \!

---
+++ squarespace_hack.rb
@@ -0,0 +1,52 @@
+require "yaml"
+require 'rubygems'
+require 'twitter'
+require 'net/http'
+
+class Squarespace_hack
+  def initialize(user_name)
+    p "initializing"
+    @user_name = user_name
+    load_twitter_config_for
+    get_twitter_session
+    run
+  end
+  
+  def load_twitter_config_for
+    p "loading twitter config"
+    @twitter_config = YAML::load(File.open("twitter.yaml"))
+    @app = @twitter_config['application']
+    @config = @twitter_config[@user_name]
+  end
+    
+  def get_twitter_session
+    p "getting a twitter session"    
+    
+    httpauth = Twitter::HTTPAuth.new(@config['email'], @config['password'])
+    @client = Twitter::Base.new(httpauth)
+  end
+  
+  def winner?
+    @client.replies.each {|tweet| return true if tweet.user.screen_name.downcase == "squarespace" }
+    return false
+  end
+  
+  def random_twitt
+    text = Net::HTTP.get(::URI.parse("http://iheartquotes.com/api/v1/random?format=raw&max_lines=4&max_characters=120&show_permalink=false&show_source=false"))
+    return text + " #squarespace"
+  end
+  
+  def run
+    p "go !"
+    while not winner? do
+      text =  random_twitt
+      p 'twitting : ' + text
+      @client.update(text)
+      sleep 120
+    end
+    p "WE WON !!!!!!!"
+  end
+end
+
+
+Squarespace_hack.new('fake_meuble')
